c644b8f5ad7840792e40b2ac895c85057c567729
Jason Dudash <thenut77@gmail.com>
2015-01-24 11:29:24 -0500

    adding the beginnings of the String extension class

---
+++ Source/String.swift
@@ -0,0 +1,59 @@
+//
+//  String.swift
+//  SwiftKick
+//
+//  Created by Jason on 1/24/15.
+//
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2015 Jason Dudash
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+//
+
+
+// Reference to Swift Strings:
+// https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/StringsAndCharacters.html
+
+import Foundation
+
+extension String
+{
+    
+    //-----------------------------------------------------------------------------------------------
+    public func encodeForURL() -> String?
+    {
+        return self.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())
+    }
+
+    //-----------------------------------------------------------------------------------------------
+    public func toBase64() -> NSString?
+    {
+        let utf8string = self.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion:false)
+        return utf8string?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.Encoding64CharacterLineLength)
+    }
+    
+    //-----------------------------------------------------------------------------------------------
+    public func fromBase64() -> NSString?
+    {
+        let base64data = NSData(base64EncodedString: self, options: NSDataBase64DecodingOptions.IgnoreUnknownCharacters)
+        if base64data == nil { return nil }
+        return NSString(data: base64data!, encoding: NSUTF8StringEncoding)
+    }
+}
--- SwiftKickPlayground.playground/section-1.swift
+++ SwiftKickPlayground.playground/section-1.swift
@@ -2,6 +2,9 @@
 
 import SwiftKick
 
+//-----------------------------------------------------------------------------------------------
+// Logger
+//-----------------------------------------------------------------------------------------------
 // Set a log level to restrict output, and a verbosity of the output
 let log = SwiftKick.Logger(level: Logger.LoggerLevel.WARN, verbosity:Logger.LoggerVerbosity.DEFAULT)
 
@@ -12,3 +15,17 @@ log.info("TESTING INFO")
 log.warn("TESTING WARN")
 log.error("TESTING ERROR")
 log.fatal("TESTING FATAL")
+
+
+//-----------------------------------------------------------------------------------------------
+// String
+//-----------------------------------------------------------------------------------------------
+// build a URL string
+let myString:String = "http:\\\\Make/this into a @URL string"
+let myStringURL = myString.encodeForURL()
+
+// base64 encode a string
+let someString = "This is a string I'd like like to encode"
+let b64String:String = someString.toBase64()!
+b64String.fromBase64()
+
